4e821c3b779a01eff517f18be5a1cb535f414666
Shirisha Reddy <114852869+shirisha-1910@users.noreply.github.com>
2024-11-26 19:12:02 +0530

    Update RetailStoreApplication.java

--- src/main/java/com/example/retailstore/RetailStoreApplication.java
+++ src/main/java/com/example/retailstore/RetailStoreApplication.java
@@ -8,8 +8,22 @@ public class RetailStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetailStoreApplication.class, args);
-		SpringApplication.run(RetailStoreApplication.class, args);
-
 	}
 
 }
+
+
+package com.example.retailstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailStoreApplication.class, args);
+	}
+
+}
+
